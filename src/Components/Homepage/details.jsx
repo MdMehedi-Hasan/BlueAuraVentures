50b3fd153cc447adf66faad0b56f02619945d05b
MdMehedi-Hasan <mh19115@gmail.com>
2024-04-24 16:23:12 +0600

    design - details section designed

--- src/Components/Homepage/details.jsx
+++ src/Components/Homepage/details.jsx
@@ -1,18 +1,24 @@
-// import 
+import image1 from '../../assets/Others/invest.jpg'
+import image2 from '../../assets/Others/startup.jpg'
 
 export default function details() {
-  return (
-      <div>
-          <ul>
-              <li>
-                  <img src="" alt="" />
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, inventore quisquam. Fugiat sunt obcaecati enim consectetur impedit dolores ullam voluptatum!</p>
-              </li>
-              <li>
-                  <img src="" alt="" />
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, inventore quisquam. Fugiat sunt obcaecati enim consectetur impedit dolores ullam voluptatum!</p>
-              </li>
-          </ul>
-    </div>
-  )
+    return (
+        <div>
+            <ul className='max-w-5xl mx-auto'>
+                <li className='flex items-center gap-8 bg-gray-100 rounded'>
+                    <img src={image1} alt="" className='w-96 rounded-l'/>
+                    <p className='text-xl text-sky-500 pr-5'> We seek to invest in startups addressing local problems head on, beyond the MVP
+                        stage with clear product-market fit, traction and path to scalability.
+                    </p>
+                </li>
+                <li className='flex items-center gap-8 bg-gray-100 rounded mt-10'>
+                    <img src={image2} alt="" className='w-96 rounded-l'/>
+                    <p className='text-xl text-sky-500 pr-5'>The macro and micro matter. As such, we seek to invest in startups that can identify
+                        opportunities led by macrotrends and digitization, while actively exploiting local
+                        business opportunities with a focus on growth.
+                    </p>
+                </li>
+            </ul>
+        </div>
+    )
 }
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import Footer from "../Components/Homepage/Footer";
 import Header from "../Components/Homepage/Header";
 import Portfolio from "../Components/Homepage/Portfolio";
+import Details from "../Components/Homepage/Details"
 
 export default function Homepage() {
     return (
         <div>
             <Header />
+            <Details/>
             <Portfolio />
             <Footer/>
         </div>
