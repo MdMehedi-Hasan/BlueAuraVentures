62f97f385d732a80cd1897e2ba48e58a920d4ed4
MdMehedi-Hasan <mh19115@gmail.com>
2024-04-26 12:23:28 +0600

    design - mod

--- src/Components/Homepage/Pillars.jsx
+++ src/Components/Homepage/Pillars.jsx
@@ -7,7 +7,7 @@ export default function Pillars() {
     return (
         <div className='pt-20 pb-28 px-3 md:px-0'>
             {/* <h1 className='text-center font-bold text-xl sm:text-2xl md:text-4xl text-[#0096c7] mb-3' id="about">Key Pillars</h1> */}
-            <p className='text-center mb-10 text-[#0096c7] text-xl sm:text-2xl md:text-3xl' id="about">These are the key pillars <br /> that define our investment ethos and approach</p>
+            <p className='text-center mb-10 text-[#0096c7] text-xl sm:text-2xl md:text-3xl' id="about">These are the key pillars <br /> that define our investment ethos and approach.</p>
             <ul className='flex flex-wrap justify-center gap-10'>
                 <li className='flex flex-col items-center'>
                     <div className='bg-[#0096c7] rounded-full p-6 w-fit mb-2'>
